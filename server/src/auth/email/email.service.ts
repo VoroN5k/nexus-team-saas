80a75c17c9f78e105724d67be6c229a07515dca7
Artem <74196886+VoroN5k@users.noreply.github.com>
2026-03-16 10:58:46 +0000

    fix(email): correct typo in sendVerificationEmail parameter name

--- server/src/auth/email/email.service.ts
+++ server/src/auth/email/email.service.ts
@@ -5,7 +5,7 @@ import { MailerService } from "@nestjs-modules/mailer";
 export class EmailService {
     constructor(private readonly mailer: MailerService) {}
 
-    async sendVerificationEmail(email:string, toke: string) {
+    async sendVerificationEmail(email:string, token: string) {
         const verificationLink = `http://localhost:4000/api/auth/verify-email?token=${token}`;
         await this.mailer.sendMail({
             to: email,
