3f18f2ead2b6fc3f3251397b83042824c538d5b6
Artem <74196886+VoroN5k@users.noreply.github.com>
2026-03-16 10:49:56 +0000

    feat: add email verification functionality

---
+++ server/src/auth/email/email.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { MailerService } from "@nestjs-modules/mailer";
+
+@Injectable()
+export class EmailService {
+    constructor(private readonly mailer: MailerService) {}
+
+    async sendVerificationEmail(email:string, toke: string) {
+        const verificationLink = `http://localhost:4000/api/auth/verify-email?token=${token}`;
+        await this.mailer.sendMail({
+            to: email,
+            subject: "Verify your email",
+            html: `<p>Click <a href="${verificationLink}">here</a> to verify your email.</p>`
+        });
+    }
+}
