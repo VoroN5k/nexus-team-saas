8cb6a20c50d2c8b67d7749d12bbc1cf9eb126316
Artem <74196886+VoroN5k@users.noreply.github.com>
2026-03-16 10:03:05 +0000

    refactor: remove AppController and AppService, introduce AuthService

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [AuthService],
 })
 export class AppModule {}
