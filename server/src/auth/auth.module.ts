8b9a3a401ff0d07d57134258f36d8523a55fbdb8
VoroN5k <Volovykartem@gmail.com>
2026-04-18 00:30:19 +0300

    feat(opaque): add OpaqueService to AuthModule providers and exports

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { JwtModule } from "@nestjs/jwt";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { JwtStrategy } from "./strategies/jwt.strategy";
+import { OpaqueService } from 'src/auth/opaque/opaque.service';
 
 @Module({
     imports: [
@@ -15,14 +16,14 @@ import { JwtStrategy } from "./strategies/jwt.strategy";
         JwtModule.registerAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
+            useFactory: (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET'),
                 signOptions: { expiresIn: '7d' },
             }),
         }),
     ],
     controllers: [AuthController],
-    providers: [AuthService, JwtStrategy],
-    exports: [JwtModule]
+    providers: [AuthService, JwtStrategy, OpaqueService],
+    exports: [JwtModule, OpaqueService]
 })
 export class AuthModule {}
